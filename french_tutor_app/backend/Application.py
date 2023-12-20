22b32e1464ed7d22781c3e588d49161bc774637c
nathanvdv <nathan.vandeven00@gmail.com>
2023-12-20 15:55:47 +0100

    Remove unused imports and refactor translation API key

--- french_tutor_app/backend/Application.py
+++ french_tutor_app/backend/Application.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pyphen
 import pandas as pd
 import streamlit as st
 import textstat
@@ -8,7 +7,6 @@ from joblib import load
 from lexicalrichness import LexicalRichness
 from nltk.tokenize import word_tokenize, sent_tokenize
 import re
-
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from transformers import CamembertTokenizer, CamembertModel
@@ -121,6 +119,7 @@ def enhance_dataset(data):
     enhanced_data = data.join(features_df).join(embeddings_df).drop(['embeddings', 'sentence'], axis=1)
     return enhanced_data
 
+
 def explain_prediction(sentence, model, scaler, enhanced_test_data):
     feature_vector = enhanced_test_data[df['sentence'] == sentence].iloc[0]
 
@@ -145,7 +144,6 @@ def explain_prediction(sentence, model, scaler, enhanced_test_data):
 
     return combined_html
 
-import requests
 
 def translate_text(text, source_language='fr', target_language='en'):
     url = "https://google-translate1.p.rapidapi.com/language/translate/v2"
@@ -159,7 +157,7 @@ def translate_text(text, source_language='fr', target_language='en'):
     headers = {
         "content-type": "application/x-www-form-urlencoded",
         "Accept-Encoding": "application/gzip",
-        "X-RapidAPI-Key": "b64f294ccbmsh857601b84d731bap1ee52cjsn58a7ef880106",
+        "X-RapidAPI-Key": "9b09c373e6mshec85ea892a9c7b5p172ebfjsn69a513583778",
         "X-RapidAPI-Host": "google-translate1.p.rapidapi.com"
     }
 
@@ -171,7 +169,6 @@ def translate_text(text, source_language='fr', target_language='en'):
     else:
         print("Error:", response.text)  # Print error for debugging
         return "Translation error."
-
 
 
 def predict_text(text):
@@ -199,10 +196,7 @@ def predict_text(text):
     cefr_mapping = {0: 'A1', 1: 'A2', 2: 'B1', 3: 'B2', 4: 'C1', 5: 'C2'}
     df['predicted_difficulty'] = [cefr_mapping[pred] for pred in predictions]
 
-    st.write("Prediction mapped", df)
-
-    return df['predicted_difficulty']
-
+    return df[['sentence', 'predicted_difficulty']], df['predicted_difficulty'].mode()[0]
 
 
 def learning_tips(file_path):
@@ -239,20 +233,28 @@ def learning_tips(file_path):
 
 
 def show_learning_tips(difficulty_level):
-    string_to_show = "Learning Tips for dificulty level: " + str(difficulty_level)
+    string_to_show = "Learning Tips for difficulty level: " + str(difficulty_level)
+    
+    if difficulty_level in ['A1', 'A2', 'B1', 'B2', 'C1', 'C2']:
+        string_to_show += "\n\n"
+        string_to_show += "\n\nVocabulary: These are vocabulary items you might look into to enhance your language skills at this level.\n\n"
+        string_to_show += "Orthography: Tips on how you could practice and improve your writing skills.\n\n"
+        string_to_show += "Grammar and Conjugation: Essential grammar rules and conjugation patterns necessary for this difficulty level.\n\n"
+        string_to_show += "Speaking Subjects: Recommended topics and subjects for speaking practice suitable for your language proficiency."
+    else:
+        string_to_show += "\n\nDifficulty level not recognized."
+
     st.markdown(f"<h2>{string_to_show}</h2>", unsafe_allow_html=True)
+
     if difficulty_level == 'C1' or difficulty_level == 'C2':
-        st.markdown("<h3>You are already good ! Keep going !</h3>", unsafe_allow_html=True)
-        pass
+        st.markdown("<h3>You are already good! Keep going!</h3>", unsafe_allow_html=True)
     else:
         file_name = f"french_tutor_app/backend/data/data{difficulty_level}.csv"
         learning_tips(file_name)
-    # Include your learning tips here
 
 
 def main():
     st.markdown("<h1 style='text-align: center; color: grey;'>Group Nvidia</h1>", unsafe_allow_html=True)
-
     st.markdown("<h2 style='text-align: center; color: black;'>French Tutor App </h2>", unsafe_allow_html=True)
 
     col1, col2, col3 = st.columns(3)
@@ -276,17 +278,23 @@ def main():
             st.write(f"Translated Text: {translated_text}")
         else:
             st.warning("Please enter some text before translating.")
-            
-    # Prediction button
-    if st.button("Evaluate my level") & st.checkbox("Get Learning Tips"):
+
+    display_learning_tips = st.checkbox("Get Learning Tips")
+
+    # Evaluate my level button
+    if st.button("Evaluate my level"):
         if input_text:
-            prediction = predict_text(input_text)
-            show_learning_tips("B2")
+            sentences_df, most_frequent_difficulty = predict_text(input_text)
+
+            # Display the table with sentences and their predicted difficulty
+            st.table(sentences_df)
+
+            st.write(f"Your predicted level is (averaged and rounded downwards): {most_frequent_difficulty}")
+
+            if display_learning_tips:
+                show_learning_tips(most_frequent_difficulty)
         else:
             st.warning("Please enter some text before predicting your level.")
-    else:
-        if input_text:
-            predict_text(input_text)
 
 
 if __name__ == "__main__":
